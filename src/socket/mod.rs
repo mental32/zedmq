678f0e414cc8390fe42044f105bddf28fca8b2c1
mental <m3nta1@yahoo.com>
2020-09-13 17:47:58 +0100

    Add push/pull files

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -8,7 +8,15 @@ use router::RouterType;
 pub struct Socket;
 
 impl Socket {
-    pub fn router() -> SocketBuilder<RouterType> {
+    pub fn router<'a>() -> SocketBuilder<'a, RouterType> {
+        SocketBuilder::new()
+    }
+
+    pub fn push<'a>() -> SocketBuilder<'a, RouterType> {
+        SocketBuilder::new()
+    }
+
+    pub fn pull<'a>() -> SocketBuilder<'a, RouterType> {
         SocketBuilder::new()
     }
 }
--- src/socket/router.rs
+++ src/socket/router.rs
@@ -1,9 +1,13 @@
 use super::builder::{IsARealSocketType, SocketBuilder};
 
 impl IsARealSocketType for RouterType {
-    fn constructor(_: &()) -> Result<Self, ()> { Err(()) }
+    fn construct_from(_: &()) -> Result<Self, ()> {
+        Err(())
+    }
 
-    fn check_build_information(_: &SocketBuilder<Self>) -> Result<(), ()> { Err(()) }
+    fn check_build_information(_: &SocketBuilder<Self>) -> Result<(), ()> {
+        Err(())
+    }
 }
 
 pub struct RouterType;

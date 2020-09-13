45427fbb9cb9687dc1507796adb9976c511eb2bd
mental <m3nta1@yahoo.com>
2020-09-13 17:47:41 +0100

    Add `SocketBuilder::within`, `bind`, and `connect` methods

--- src/socket/builder.rs
+++ src/socket/builder.rs
@@ -1,29 +1,58 @@
 use core::marker::PhantomData;
 
-pub  trait IsARealSocketType: Sized {
-    fn constructor(state: &()) -> Result<Self, ()>;
+use crate::context::Context;
+
+pub trait IsARealSocketType: Sized {
+    fn construct_from(state: &()) -> Result<Self, ()>;
 
     fn check_build_information(builder: &SocketBuilder<Self>) -> Result<(), ()>;
 }
 
-pub struct SocketBuilder<S: IsARealSocketType> {
-    _raw_type: PhantomData<S>,
+enum Position<'a> {
+    Connect(&'a str),
+    Bind(&'a str),
 }
 
-impl<S: IsARealSocketType> SocketBuilder<S> {
+pub struct SocketBuilder<'a, S: IsARealSocketType> {
+    _raw_type: PhantomData<S>,
+    position: Option<Position<'a>>,
+}
+
+impl<'a, S: IsARealSocketType> SocketBuilder<'a, S> {
     pub(crate) fn new() -> Self {
         Self {
             _raw_type: PhantomData,
+            position: None,
         }
     }
 
+    pub fn within(self, context: &Context) -> Self {
+        self
+    }
+
+    pub fn bind(mut self, address: &'a str) -> Self {
+        self.position = Some(Position::Bind(address));
+        self
+    }
+
+    pub fn connect(mut self, address: &'a str) -> Self {
+        self.position = Some(Position::Connect(address));
+        self
+    }
+
     pub async fn build(self) -> Result<S, ()> {
+        // Do build stuff
+
         S::check_build_information(&self)?;
 
-        // Do build stuff
         // i.e. tcp connect/listen
+        match self.position {
+            None => return Err(()),
+            Some(Position::Connect(endpoint)) => (),
+            Some(Position::Bind(endpoint)) => (),
+        }
 
-        // S::constructor(build_state)
+        // S::construct_from(build_state)
         Err(())
     }
 }

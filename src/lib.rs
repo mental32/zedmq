48a96e874314da211ddcefedb4ab0dff336d172d
mental <m3nta1@yahoo.com>
2020-09-14 18:16:33 +0100

    Rewrite endpoint module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
-// mod endpoint;
 pub mod codec;
 pub mod context;
+pub mod endpoint;
 pub mod socket;
 
 pub mod prelude {
-    pub use super::codec::frame::{Frame, RawFrame};
+    pub use super::codec::{
+        frame::{Frame, RawFrame},
+        owned_frame::OwnedFrame,
+    };
     pub use super::context::Context as ZmqContext;
     pub use super::socket::Socket as ZmqSocket;
 }
